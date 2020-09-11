bc0820f2066ef5ef839eab7fa13e65083c3196eb
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-11 17:40:22 +0200

    test

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 
 bot.on('message', function (message) {
-  if (message.content == '!tutoriel') {
-        message.channel.send('Vive les tutos');
-  }
-});
+  if (message.content.startswith('.tempstrajet')
+            const args = message.content.slice(message.lenght).split(/ +/);
+              message.channel.send('$args[1]');
 
-bot.on('message', function (message) {
-  if (message.content.startswith('.tempstrajet') {
-      const args = message.content.slice(message.lenght).split(/ +/);
-        message.channel.send( $args[1] );
-  }
-});
+  };
+);
+
+
 
 
 bot.login(process.env.Token);
