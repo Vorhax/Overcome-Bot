b1a337aaa1f52528d42b111fa3bac3bbc69943ae
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-14 17:09:29 +0200

    Update kmtime.js

--- Commands/kmtime.js
+++ Commands/kmtime.js
@@ -1,6 +1,44 @@
 module.exports.run = (bot, msg, args) => {
+    if(args.length > 2 || args.length < 1) return msg.channel.send("Il n'y pas assez d'argument /n Vous devez donner au moins un nombre de kilometre par default la vitesse utilisé sera 30.000km/h")
+    if(args.length == 1){
+      var secondes = args[0] / 30000
+      var retour='';
+       if(secondes<0)
+       {
+            retour='-';
+            secondes*=-1;
+       }
+       if(secondes>=3600*24)
+       {
+            var jours=Math.floor(secondes/3600/24);
+            retour+=jours+'j ';
+            secondes-=jours*3600*24;
+       }
+       if(secondes>=3600)
+       {
+            var heures=Math.floor(secondes/3600);
+            retour+=heures+'h ';
+            secondes-=heures*3600;
+       }
+       if(secondes>=60)
+       {
+            var minutes=Math.floor(secondes/60);
+            retour+=minutes+'m ';
+            secondes-=minutes*60;
+       }
+       if(secondes>0)
+       {
+            retour+=secondes+'s';
+       }
 
-    msg.channel.send('on va faire le calcul ici.' + msg)
+
+
+      var contenu =   `temps passé : ${retour} `
+    }
+
+
+
+    msg.channel.send(contenu)
   }
 
 module.exports.help =  {
