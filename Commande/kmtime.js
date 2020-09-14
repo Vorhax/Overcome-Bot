7f87dd2bdb109b9ff7802b389362095dfb068185
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-14 09:52:09 +0200

    mise en place des commandes

---
+++ Commande/kmtime.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Kmtime',
+  description: 'Calcule temps de trajet en fonction d\'une vitesse et d\'un nombre de km.',
+  execute(message) {
+    message.channel.send('on va faire le calcul ici.');
+  }
+};
--- index.js
+++ index.js
@@ -1,23 +1,47 @@
-
-
 const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); // on  charge le dossier avec toutes les commandes du BOT
+for (const file of commandFiles) { // on  charge le dossier avec toutes les commandes du BOT
+  const command = require(`./commands/${file}`); // on  charge le dossier avec toutes les commandes du BOT
+  client.commands.set(command.name, command); // on  charge le dossier avec toutes les commandes du BOT
+} // on  charge le dossier avec toutes les commandes du BOT
+
+
 bot.on('ready',async() => {
     console.log('Bot lancé');
-    bot.user.setActivity('Bot De calcule Overcome');
+    bot.user.setActivity('Bot De calcul Overcome');
 
 });
-//
-bot.on('message', async (msg) => {
-  if (msg.content.startsWith('.tempstrajet') && !msg.author.bot){
-              msg.channel.send('test')
-              args = msg.content.substring(1).split(" ")
-              cmd = args[1]
-              //retour = args[1]
-              msg.channel.send(cmd)
-          }
-  });
+//TEST PERSO
+//bot.on('message', async (msg) => {
+//  if (msg.content.startsWith('.tempstrajet') && !msg.author.bot){
+//              msg.channel.send('test')
+//              args = msg.content.substring(1).split(" ")
+//              cmd = args[1]
+//              //retour = args[1]
+//              msg.channel.send(cmd)
+//          }
+//  });
+
+// On récupere le message tapé dans le chat et on l'envoie au bon fichier de commande
+
+
+  bot.on('message', message => {
+  if (!message.content.startsWith('.') || message.author.bot) return;
+
+    const args = message.content.slice(1).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+      client.commands.get(command).execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply("Une erreur s'est produite pendant l'exécution de la commande !");
+    }
+})
 
 bot.login(process.env.Token);
 
