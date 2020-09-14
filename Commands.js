936cdd8904bdf6d1cc930c05888451292eb6f77b
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-14 15:32:22 +0200

    Update Commands.js

---
+++ Commands.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const commands = [];
+
+const commandFactory = (command) => {
+  const {run} = command;
+  const {name} = command.help;
+
+  return{
+    name,
+    run
+  };
+};
+
+const getCommand = (name) => {
+    return command.find((command) => command.name === name);
+};
+
+const getComandList = () => {
+  return commands.map((command) => command.name).join(', ');
+};
+
+fs.reddir('./Commands/',(err, files)=>{
+  if(err) console.error(err);
+  const commandFiles = files.filter(f => f.indexOf('.js') !== -1);
+
+  commandFiles.forEach((commandFiles) => {
+    const command = require(`./commands/$(commandFiles)`);
+
+    commands.push(command);
+  });
+
+});
+
+module.exports.getCommand = getCommand;
+module.exports.getCommandList = getCommandList;
