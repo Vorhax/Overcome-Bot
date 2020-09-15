b7f4790fee75a437e9015a1253603ca8e46328b3
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-15 14:14:42 +0200

    Update kmsu.js

--- Commands/kmsu.js
+++ Commands/kmsu.js
@@ -1,3 +1,6 @@
+const Discord = require ('discord.js');
+
+
 module.exports.run = (bot, msg, args) => {
 
 
@@ -9,8 +12,15 @@ module.exports.run = (bot, msg, args) => {
   retour+=su.toFixed(2)+' SU';
 
 
-  var contenu = `>>> **Distance :** ${retour}`
-  msg.channel.send(contenu)
+  var contenu = `** ${args[0]} km donne Distance : ${retour}**`
+
+
+  let hEmbed = new Discord.MessageEmbed()
+  .setColor('26a1d1')
+  .setTitle(contenu)
+  .setFooter("Overcome vous souhaite une bonne journée")
+  .setTimestamp();
+  msg.channel.send(hEmbed)
 }
 
 module.exports.help =  {
