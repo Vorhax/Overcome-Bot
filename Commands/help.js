e7e792db6e102195b27e5f28f5f508e7200bb768
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-15 08:22:21 +0200

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -2,7 +2,7 @@ const Discord = require ('discord.js');
 
 module.exports.run = async (bot,msg,args) => {
     let hEmbed = new Discord.MessageEmbed()
-    .setcolor('26a1d1')
+    .setColor('26a1d1')
     .setTitle('Aide du bot')
     .setDescription('mettre ici le descriptif du bot')
     .addField('Commande : ', "- **.kmsu** => Converti des kilomètre en SU")
