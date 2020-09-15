f6637c25f262036d61629079fd08f019014c128e
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-15 14:08:12 +0200

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -4,9 +4,13 @@ module.exports.run = async (bot,msg,args) => {
     let hEmbed = new Discord.MessageEmbed()
     .setColor('26a1d1')
     .setTitle('Aide du bot')
-    .setDescription('Je suis a votre service vous trouverez ci-dessous la liste des commandes possible')
-    .addField('Commande : ', "- **.kmsu** => Converti des kilomètre en SU\n")
-    .addField('Commande : ', "- **.kmsu** => Converti des kilomètre en SU")
+    .setDescription('Je suis à votre service vous trouverez ci-dessous la liste des commandes possibles')
+    .addField('=> .kmsu ', "Converti des kilomètre en SU \n Parametre d'entrée : **<distance en kilomètre>**\n")
+    .addField('=> .kmtime ', "Donne le temps de trajet pour une distance donnée à une vitesse donnée \n Parametre d'entrée : **<Distance en kilomètre>** - **(<Vitesse en Km/h>)** (si pas de valeur 30.000km/h)\n")
+    .addField('=> .sukm ', "Converti des SU en Kilomètre \n Parametre d'entrée : **<distance en SU>**\n")
+    .addField('=> .sutime ', "Donne le temps de trajet pour une distance donnée à vitesse donnée \n Parametre d'entrée : **<Distance en SU >** - **(<Vitesse en Km/h>)** (si pas de valeur 30.000km/h)\n")
+    .addField('=> .timesu ', "Donne la vitesse à avoir pour faire le trajet en un temps donné \n Parametre d'entrée : **<Distance en SU>** - **<Temps en minutes>** \n")
+    .addField('=> .warp ', "Donne une estimation des cellules warp consommé pour un trajet en warp drive \n Parametre d'entrée : **<distance en SU>** - **<Poids en tonne>**\n")
     .setFooter("Overcome vous souhaite une bonne journée")
     .setTimestamp();
 
