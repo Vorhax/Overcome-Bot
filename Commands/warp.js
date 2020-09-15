34bca24b89577490ab4b15aeb41dc9d2f6c00f03
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-15 09:45:28 +0200

    Update warp.js

--- Commands/warp.js
+++ Commands/warp.js
@@ -1,8 +1,8 @@
 module.exports.run = (bot, msg, args) => {
 
     if(args.length > 2 || args.length < 1) return msg.channel.send("Il n'y pas assez d'argument \nVous devez donner au moins un nombre de SU et le poids en Tonne du vaisseau")
-    if(args.length==1 && (isNaN(args[0]) || args[0] = 0)) return msg.channel.send("L'argument est incorrect merci de renseigner un chiffre supérieur à 0.")
-    if(args.length==2 && (isNaN(args[0])||isNaN(args[1])) && (args[0]=0 || args[1] = 0)) return msg.channel.send("Les arguments sont incorrects merci de renseigner des chiffres supérieur à 0.")
+    if(args.length==1 && (isNaN(args[0]) || args[0] == 0)) return msg.channel.send("L'argument est incorrect merci de renseigner un chiffre supérieur à 0.")
+    if(args.length==2 && (isNaN(args[0])||isNaN(args[1])) && (args[0]==0 || args[1] == 0)) return msg.channel.send("Les arguments sont incorrects merci de renseigner des chiffres supérieur à 0.")
 
     var distance = args[0];
     var poids = args[1];
