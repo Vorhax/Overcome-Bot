28de66218de505bbf9e8c394300596002c647c7e
Vorhax <71125871+Vorhax@users.noreply.github.com>
2020-09-15 11:50:09 +0200

    Update help.js

--- Commands/help.js
+++ Commands/help.js
@@ -4,7 +4,8 @@ module.exports.run = async (bot,msg,args) => {
     let hEmbed = new Discord.MessageEmbed()
     .setColor('26a1d1')
     .setTitle('Aide du bot')
-    .setDescription('mettre ici le descriptif du bot')
+    .setDescription('Je suis a votre service vous trouverez ci-dessous la liste des commandes possible')
+    .addField('Commande : ', "- **.kmsu** => Converti des kilomètre en SU\n")
     .addField('Commande : ', "- **.kmsu** => Converti des kilomètre en SU")
     .setFooter("Overcome vous souhaite une bonne journée")
     .setTimestamp();
